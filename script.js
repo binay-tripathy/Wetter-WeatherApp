fbb910db62190f3e552509db9a5228eddc0294d9
Binay Tripathy <binaytripathy123@gmail.com>
2022-11-03 01:06:35 +0530

    Completed the working of website

--- script.js
+++ script.js
@@ -9,27 +9,66 @@ const getData = async (e) => {
   if (cityName == "") {
     error.innerText = " ** Please enter city Name ";
   }
+
   else {
     try {
-      const d = new Date();
+      //fetching weather api
       fetch(`https://api.weatherapi.com/v1/current.json?key=0bab7dd1bacc418689b143833220304&q=${cityName}`)
-        .then((res) => res.json())
+        .then((res) => res.json()) 
         .then((data) => {
+
           document.getElementById("city").innerHTML = data.location.name
           document.getElementById("temp").innerHTML = data.current.temp_c + '\xB0' + 'C'
           document.getElementById("condition").innerHTML = data.current.condition.text
           document.getElementById("wind").innerHTML = data.current.wind_kph + ' km/h'
           document.getElementById("pressure").innerHTML = data.current.pressure_mb + ' mb'
-          document.getElementById("weaticon").innerHTML = data.current.condition.icon
+
+          //FETCHING CONDITIONS.JSON FILE
+          fetch('./conditions.json')
+            .then((response) => response.json())
+            .then((data1) => {
+
+              if (data.current.is_day == 1) { //IF ITS DAY THEN THIS WILL EXECUTE
+                for (var i = 0; i < data1.length; i++) {
+                  if (data1[i].code == data.current.condition.code) {
+                    var scan = document.createElement("img");
+                    scan.src = `day/${data1[i].icon}.png`;
+                    const list = document.getElementById("weaticon");
+                    while (list.hasChildNodes())
+                      list.removeChild(list.firstChild);
+                    var picsrc = document.getElementById("weaticon");
+                    picsrc.appendChild(scan);
+                  }
+                }
+              }
+              else {  //IF ITS NIGHT THEN THIS WILL EXECUTE
+                for (var i = 0; i < data1.length; i++) {
+                  if (data1[i].code == data.current.condition.code) {
+                    var scan = document.createElement("img");
+                    scan.src = `night/${data1[i].icon}.png`;
+                    const list = document.getElementById("weaticon");
+                    while (list.hasChildNodes())
+                      list.removeChild(list.firstChild);
+                    var picsrc = document.getElementById("weaticon");
+                    picsrc.appendChild(scan);
+                  }
+                }
+
+              }
+            })
         })
+
+
       error.innerText = "";
     }
     catch {
-      error.innerText = " ** Please Enter Valid City Name ";
+      // error.innerText = " ** Please Enter Valid City Name ";
       content.innerHTML = "";
     }
   }
 }
+
+//TIME FUNCTION
 setInterval(date, 1000);
 
 function date() {
